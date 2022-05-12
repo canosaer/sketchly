9f2cb5752f948ecf16fe37ea8b72b45cd0b6b94e
canosaer <67963828+canosaer@users.noreply.github.com>
2022-05-12 08:53:47 -0400

    more

--- sketchly-server/index.js
+++ sketchly-server/index.js
@@ -2,27 +2,27 @@ const express = require('express')
 const cors = require('cors')
 const { randomUUID } = require('crypto')
 const app = express()
+require('dotenv').config()
 
 const mongoose = require('mongoose')
 
 const GameModel = require('./models/Games')
 
-mongoose.connect('mongodb+srv://canosaer:CqKa$bFc5QyJ5CH@cluster0.b8iax.mongodb.net/sketchly?retryWrites=true&w=majority', {useNewUrlParser: true})
+mongoose.connect(process.env.DATABASE, {useNewUrlParser: true})
 
 
-const port = 1337
+const port = process.env.PORT || 1337
 
-//creates front-end
 app.use(express.static('blackbox-client'))
 app.use(express.json())
 app.use(cors())
 
 app.get('/insert', async (req,res) => {
   const game = new GameModel({
-    name: 'brave traveler',
-    contributors: ['Mr. Robot', 'Whiterose'],
+    name: 'nightman',
+    contributors: ['Dayman', 'Charlie'],
     turn: 2,
-    phrases: 'Evil Corp',
+    phrases: 'Troll Toll',
     images: [[{
       color: "black",
       time: 1652313622969,
