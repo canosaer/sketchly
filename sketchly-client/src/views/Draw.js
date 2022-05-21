43b0fecc7da169ea23cae520d3cc6bc88a6946f1
canosaer <67963828+canosaer@users.noreply.github.com>
2022-05-21 18:59:59 -0400

    draw styles

--- sketchly-client/src/views/Draw.js
+++ sketchly-client/src/views/Draw.js
@@ -12,6 +12,7 @@ export default function Draw() {
     const [state, dispatch] = useContext(Context)
     const [quitMenuOpen, setQuitMenuOpen ] = useState(false)
     const [penColor, setPenColor] = useState('black')
+    const [eraseMode, setEraseMode] = useState(false)
 
     const ref = useRef()
 
@@ -51,7 +52,7 @@ export default function Draw() {
         <>
             <main className="draw">
                 <header className="header">
-                    <button className="header__button header__button_erase" onClick={() => ref.current.clear()} ><FontAwesomeIcon className="header__icon header__icon_erase" icon={"eraser"} /></button>
+                    <button className="header__button header__button_reset" onClick={() => ref.current.clear()} ><FontAwesomeIcon className="header__icon header__icon_reset" icon={"rotate-right"} /></button>
                     <Timer expiryTimestamp={time} />
                     <button className="header__button header__button_quit" onClick={() => setQuitMenuOpen(!quitMenuOpen)}><FontAwesomeIcon className="header__icon header__icon_quit" icon={"xmark"} /></button>
                     <div className={quitStyles}>
@@ -66,7 +67,7 @@ export default function Draw() {
                 </header>
                 <SignatureCanvas 
                     ref={ref}
-                    penColor={penColor}
+                    penColor={eraseMode ? 'white' : penColor}
                     canvasProps={{
                         // width: useWindowDimensions().width, 
                         width: 414,
@@ -74,19 +75,20 @@ export default function Draw() {
                         className: 'draw__canvas'
                     }}
                     backgroundColor='rgb(255,255,255)'
+                    maxWidth={eraseMode ? 50 : 2.5}
                 />
                 <section className="draw__bottom-row">
                     <div className="palette">
-                        <figure className="palette__color palette__color_black" onClick={() => setPenColor('black')}></figure>
-                        <figure className="palette__color palette__color_gray" onClick={() => setPenColor('gray')}></figure>
-                        <figure className="palette__color palette__color_blue" onClick={() => setPenColor('blue')}></figure>
-                        <figure className="palette__color palette__color_green" onClick={() => setPenColor('green')}></figure>
-                        <figure className="palette__color palette__color_red" onClick={() => setPenColor('red')}></figure>
-                        <figure className="palette__color palette__color_yellow" onClick={() => setPenColor('yellow')}></figure>
-                        <figure className="palette__color palette__color_orange" onClick={() => setPenColor('orange')}></figure>
-                        <figure className="palette__color palette__color_purple" onClick={() => setPenColor('purple')}></figure>
-                        <figure className="palette__color palette__color_pink" onClick={() => setPenColor('pink')}></figure>
-                        <figure className="palette__color palette__color_brown" onClick={() => setPenColor('brown')}></figure>
+                        <figure className="palette__color palette__color_black" onClick={() => {setEraseMode(false); setPenColor('black')}}></figure>
+                        <figure className="palette__color palette__color_gray" onClick={() => {setEraseMode(false); setPenColor('gray')}}></figure>
+                        <figure className="palette__color palette__color_blue" onClick={() => {setEraseMode(false); setPenColor('blue')}}></figure>
+                        <figure className="palette__color palette__color_green" onClick={() => {setEraseMode(false); setPenColor('green')}}></figure>
+                        <figure className="palette__color palette__color_red" onClick={() => {setEraseMode(false); setPenColor('red')}}></figure>
+                        <figure className="palette__color palette__color_orange" onClick={() => {setEraseMode(false); setPenColor('orange')}}></figure>
+                        <figure className="palette__color palette__color_purple" onClick={() => {setEraseMode(false); setPenColor('purple')}}></figure>
+                        <figure className="palette__color palette__color_pink" onClick={() => {setEraseMode(false); setPenColor('pink')}}></figure>
+                        <figure className="palette__color palette__color_brown" onClick={() => {setEraseMode(false); setPenColor('brown')}}></figure>
+                        <figure className="palette__color palette__color_erase" onClick={() => setEraseMode(!eraseMode)}><FontAwesomeIcon className="palette__icon" icon={"eraser"} /></figure>
                     </div>
                     <button className="draw__submit"><FontAwesomeIcon className="draw__submit-icon" icon={"check"} /></button>
                 </section>
